1d1172f738feb0c3032da8d16e7a78f08e4ac163
Jacquie Parker <jacquie_parker@icloud.com>
2017-06-01 20:37:40 -0700

    committing all changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,22 @@
 /* functions for general use */
+
 /* This function returns the value associated with 'whichParam' on the URL */ 
 function getURLParameters(whichParam)
 {
 	var pageURL = window.location.search.substring(1);
 	var pageURLVariables = pageURL.split('&');
-	for(var i = O; i < pageURLVariables[i].length; i++){
+	for(var i = 0; i < pageURLVariables.length; i++){
 	    var parameterName = pageURLVariables[i].split('=');
-	    if(paramterName[O] == whichParam){
+	    if(paramterName[0] == whichParam){
 	        return parameterName[1];
 	    }
 	}
 }
 
-
 var username = getURLParameters('username');
 if('undefined' == typeof username || !username){
     username = 'Anonymous_'+Math.random();
-}
+});
 
 
 $('#messages').append('<h4>'+username+'</h4>');
